c3fa797ae23b7d2eacb29f0001464e125e2db4f8
gadriwau <gadriwau@mcmaster.ca>
2018-03-22 20:29:59 -0600

    sorting works

--- assign3/assignment3.c
+++ assign3/assignment3.c
@@ -4,9 +4,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/mman.h> /*for mmap() function*/
+#include <string.h> /*for memcpy function*/
+#include <fcntl.h> /*for file descriptors*/
 
-#define BUFFER_SIZE 4
-#define NUMBER_OF_REQUESTS 300
+#define INT_SIZE 4
+#define NUMBER_OF_REQUESTS 20
+#define MEMORY_SIZE NUMBER_OF_REQUESTS*INT_SIZE
+
+// insertion sort, returns sorted array
+void sort(int requests[]) {
+	int i, j, k;
+	for (i = 1; i < NUMBER_OF_REQUESTS; i++) {
+		j = i-1;
+		k = requests[i];
+		while (k < requests[j] && j >= 0) {
+			requests[j+1] = requests[j];
+			j = j - 1;
+		}
+		requests[j+1] = k;
+	}
+}
 
 int main(int argc, char *argv[]) {
 
@@ -18,25 +36,27 @@ int main(int argc, char *argv[]) {
 	
 	int headPosition = atoi(argv[1]);
 	char* headDirection = argv[2];
-	printf("%d, %s\n", headPosition, headDirection);
 	
 	// read requests from requests.bin and store in integer array
 	int requests[NUMBER_OF_REQUESTS];
-	int i = 0;
-	char buff[BUFFER_SIZE]; // for reading from file
-	
-	FILE *fptr = fopen("request.bin", "rb");
-	while (1) {
-		if (fread(buff, sizeof(int), 1, fptr) < 1) {
-			break;
-		}
-		printf("%d\n",buff);
-		requests[i] = atoi(buff);
-		printf("here %d %d\n", atoi(buff),i);
-		i++;
+	int i; // loop counter
+	int mmapfile_fd = open("request.bin", O_RDONLY);
+	signed char *mmapfptr = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, mmapfile_fd, 0);
+	for(i = 0; i < NUMBER_OF_REQUESTS; i++){
+		memcpy(requests + i, mmapfptr + 4*i, INT_SIZE);
 	}
-	fclose(fptr);
-
+	close(mmapfile_fd);
 	
+	// sort requests
+	int sortedRequests[NUMBER_OF_REQUESTS];
+	// copy requests to sortedRequests
+	for (i = 0; i < NUMBER_OF_REQUESTS; i++) {
+		sortedRequests[i] = requests[i];
+	}
+	sort(sortedRequests);
+	for (i = 0; i < NUMBER_OF_REQUESTS; i++)
+		printf("r: %d, s: %d\n", requests[i], sortedRequests[i]);
+
+	munmap(mmapfptr, MEMORY_SIZE);
 	return 0;
 }
