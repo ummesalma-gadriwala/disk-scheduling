a049c897373b3e936167d81884c995ebe8d2e575
gadriwau <gadriwau@mcmaster.ca>
2018-04-02 11:02:12 -0600

    Test5

--- Labs/Test5/labtest5.c
+++ Labs/Test5/labtest5.c
@@ -7,19 +7,49 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <dirent.h>
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		fprintf(stderr, "usage: labtest5 <dirname string> \n");
-		return -1;
-	}
+
+int dir_traverse(char * dirname){
+	char filen[256] = "";
+	DIR *dir;
+	struct dirent *ent;
+	char * name;
+	dir = opendir(dirname); //open directory
 	
-	char* dirName = argv[1];
+	if ( dir != NULL) {
+	   printf("\n\nFiles/Directories in '%s'\n", dirname);
+	   printf("-------------------------------------------------\n");	
+	  
+	  /* print all the files and directories within directory */
+	  while ((ent = readdir (dir)) != NULL) {
+		name = ent->d_name;
+		if(strcmp(ent->d_name, ".") ==0 || strcmp(ent->d_name, "..") == 0){
+		    //ignore
+		}
+		else {
+			printf ("\nprint ent-dname=%s\n\n", name);
+			strcpy(filen, dirname);
+			strcat(filen, "/");
+			strcat(filen, name);
+			properties(filen);			    
+		}
+	  }
+	  printf("\n");	
+	  closedir(dir); //close directory
+	} 
+	else {
+	  /* could not open directory */
+	  perror ("");
+	  return EXIT_FAILURE;
+	}
+}
+
+int properties(char * dirName) {
 	struct stat fileAttributes; // variable of type stat
-	//if (stat(dirName, &fileAttributes) < 0)
-		//return -1;
-		
-	printf("%s\n", dirName);
+	if (stat(dirName, &fileAttributes) < 0)
+		return -1;
+
 	printf("File Attributes for the File/Directory: '%s'\n", dirName);
 	printf("--------------------------------------------\n");
 	printf(" Inode: %d\n", fileAttributes.st_ino);
@@ -41,6 +71,18 @@ int main(int argc, char *argv[]) {
 	printf(" Time of last access: %d\n", fileAttributes.st_atime);
 	printf(" Time of last data modification: %d\n", fileAttributes.st_mtime);
 	printf(" Last Status Change time: %d\n", fileAttributes.st_ctime);	
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "usage: labtest5 <dirname string> \n");
+		return -1;
+	}
 	
+	char* dirName = argv[1];
+	properties(dirName);
+	
+	dir_traverse(dirName);
 	return 0;
 }
